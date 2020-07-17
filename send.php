cafd909fcde7ce39e79db8e672a63501b26b33e9
ryan-w-c <cavanaghryan1@gmail.com>
2020-07-17 13:44:33 -0700

    bug fix

--- send.php
+++ send.php
@@ -17,6 +17,6 @@
         $body .= "\r\n Message: " . $userMessage;
 
         mail($to, $subject, $body);
-        header("Location: index.html?mailsent")
+        header("Location: cavanagh.dev?mailsent")
     }
 ?>
