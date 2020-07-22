c1e7a1a957cdadad92ab8ed46260b1aac7380156
ryan-w-c <cavanaghryan1@gmail.com>
2020-07-21 17:26:57 -0700

    formatiing

--- functions.js
+++ functions.js
@@ -10,13 +10,27 @@ const pic2 = document.getElementById("pop-card-2");
 // const message = document.getElementById("message");
 const formNav = document.getElementById("formNav");
 const myNav = document.getElementById("myNav");
-
+const codeInfo = document.getElementById("codeInfo");
 const start = document.getElementsByClassName("start");
 const ham = document.getElementsByClassName("ham");
 const form = document.getElementsByClassName("form");
+var info;
 
+function clicked(event, item) {
+  if(event.keyCode === 13){
+    event.preventDefault();
+    item.click();
+  }
+}
+
+function destroy(){
+  while(codeInfo.firstChild){
+    codeInfo.removeChild(codeInfo.firstChild);
+  }
+}
 
 function openNav() {
+    destroy();
     myNav.style.width = "100%";
     myNav.scrollTo(0, 0);
     link2.style.visibility = "visible";
@@ -37,7 +51,7 @@ function openNav() {
     }
   }
 
-  function closeNav() {
+  function closeNav(item, var1, var2, var3) {
     myNav.style.width = "0%";
     disable(ham);
     enable(start);
@@ -66,40 +80,59 @@ function bingoCard() {
     openNav();
     setCards('url(img/redBingo.png)', 'url(img/purpleBingo.png)');
     setLinks("https://cavanagh.dev/bingo/", "https://github.com/ryan-w-c/bingo", "Play Now", "View on Github");
-    setText("Bingo Card Generator", "This site creates a random bingo card that can be played in the browser or printed. It was created with JavaScript, CSS, and HTML.");
-}
+    setText("Bingo Card Generator", "This site creates a random bingo card that can be played in the browser or printed.");
+    createInfo("JavaScript");
+    createInfo("HTML");
+    createInfo("CSS");
+  }
 
 function elevator() {
     openNav();
     setCards('url(img/elev.png)', 'url(img/elevator.png)');
     setLinks("https://github.com/KRSung/Elevator_3.0", "https://github.com/ryan-w-c/bingo", "View on Github");
     link2.style.visibility = "hidden";
-    setText("Elevator Simulator", "This project simulates an elevator. It is capable of changing the number of floors, elevators, and seed, as well as, allowing different types of passengers. It taught the core concepts of object-oriented programming. This was a class project and completed with a partner.");
-}
+    setText("Elevator Simulator", "This project simulates an elevator. It is capable of changing the number of floors, elevators, and seed, as well as, allowing different types of passengers. It taught the core concepts of object-oriented programming and was completed with a partner as a class project.");
+    createInfo("Java");
+  }
 
 function bingoDraw() {
     openNav();
     setCards('url(img/bingoNum.png)', 'url(img/board.png)');
     setLinks("https://cavanagh.dev/bingoBoard/", "https://github.com/ryan-w-c/bingoBoard", "Play Now");
     setText("Bingo Number Draw", "This site draws random numbers for a bingo game and displays them on a bingo board.");
-}
+    createInfo("JavaScript");
+    createInfo("HTML");
+    createInfo("CSS");
+  }
 
 function rgb() {
     openNav();
     setCards('url(img/rgbPic.png)', 'url(img/rgb2.png)');
     setLinks("https://github.com/ryan-w-c/RGB-Image-Generator/releases/download/v1.0.0/RGB-Image_Generator.zip", "https://github.com/ryan-w-c/RGB-Image-Generator", "Download Zip (Windows)");
-    setText("RGB-Image-Generator", "This program was written in Python, using Kivy and Pil. It reads a user selected image and stores every RGB value from the image. A new one pixel length image is created, using the stored RGB values. The new image is then stretched to the desired output ratio. The screenshot below shows the user's image in the top left and the generated image in the top right.");
-}
+    setText("RGB-Image-Generator", "This program reads a user selected image and stores each RGB value. A new one pixel length image is created using the stored RGB values. The new image is then stretched to the desired output ratio.");
+    createInfo("Python");
+    createInfo("Kivy");
+    createInfo("PIL");
+  }
 
 function ticTacToe() {
     openNav();
     setCards('url(img/tic2.png)', 'url(img/tic1.png)');
     pic1.style.backgroundPosition = "center";
     setLinks("https://ryan-w-c.itch.io/tic-tac-toe", "https://github.com/ryan-w-c/TicTacToe", "Play Now");
-    setText("Tic-Tac-Toe", "This game was created in Unity and written in C#. It is a basic 3-in-a-row or 5-in-a-row, turn based, tic tac toe game.");
-}
+    setText("Tic-Tac-Toe", "This game is a basic 3-in-a-row or 5-in-a-row, turn based, tic-tac-toe game.");
+    createInfo("C#");
+    createInfo("Unity Game Engine");
+  }
 
-function contact() {
+function createInfo(codeUsed) {
+  info = document.createElement("p");
+  info.className = "code";
+  info.innerHTML = "&bull; " + codeUsed;
+  codeInfo.appendChild(info);
+  }
+
+function contact(item) {
     formNav.style.width = "100%";
     formNav.scrollTo(0, 0);
     // fname.value = "";
@@ -108,9 +141,9 @@ function contact() {
     // message.value = "";
     disable(start);
     enable(form);
-}
+  }
 
-function closeContact() {
+function closeContact(item) {
     formNav.style.width = "0%";
     disable(form);
     enable(start);
