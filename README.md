e50a5c5644ab58ddf4d908cfe43c6f6b625335b1
Ryan Cavanagh <cavanaghryan1@gmail.com>
2022-03-13 14:05:44 -0700

    update

--- README.md
+++
@@ -1,7 +0,0 @@
-# ryan-w-c.github.io
-First Website
-added tictactoe.html unity html game
-
-completed redesign
-added RGB-Color-Image Project
-added contact info
--- functions.js
+++ functions.js
@@ -4,57 +4,18 @@ const link1 = document.getElementById("link-1");
 const link2 = document.getElementById("link-2");
 const pic1 = document.getElementById("pop-card-1");
 const pic2 = document.getElementById("pop-card-2");
-// const fname = document.getElementById("name");
-// const email = document.getElementById("email");
-// const company = document.getElementById("company");
-// const message = document.getElementById("message");
 const formNav = document.getElementById("formNav");
 const myNav = document.getElementById("myNav");
-const codeInfo = document.getElementById("codeInfo");
-const start = document.getElementsByClassName("start");
-const ham = document.getElementsByClassName("ham");
-const form = document.getElementsByClassName("form");
-var info;
-
-function clicked(event, item) {
-  if(event.keyCode === 13){
-    event.preventDefault();
-    item.click();
-  }
-}
-
-function destroy(){
-  while(codeInfo.firstChild){
-    codeInfo.removeChild(codeInfo.firstChild);
-  }
-}
 
 function openNav() {
-    destroy();
     myNav.style.width = "100%";
     myNav.scrollTo(0, 0);
     link2.style.visibility = "visible";
     pic1.style.backgroundPosition = "unset";
-    disable(start);
-    enable(ham);
   }
   
-  function disable(item) {
-    for (var i = 0; i < item.length; i++){
-        item[i].tabIndex = "-1";
-    }
-  }
-
-  function enable(item) {
-    for (var i = 0; i < item.length; i++){
-        item[i].tabIndex = "1";
-    }
-  }
-
-  function closeNav(item, var1, var2, var3) {
+  function closeNav() {
     myNav.style.width = "0%";
-    disable(ham);
-    enable(start);
   }
 
   function setCards(x, y) {
@@ -76,75 +37,52 @@ function openNav() {
       text.append(y);
   }
 
-function bingoCard() {
+function chess() {
     openNav();
-    setCards('url(img/redBingo.png)', 'url(img/purpleBingo.png)');
-    setLinks("https://cavanagh.dev/bingo/", "https://github.com/ryan-w-c/bingo", "Play Now", "View on Github");
-    setText("Bingo Card Generator", "This site creates a random bingo card that can be played in the browser or printed.");
-    createInfo("JavaScript");
-    createInfo("HTML");
-    createInfo("CSS");
-  }
+    setCards('url(img/chess1.png)', 'url(img/chess2.png)');
+    pic1.style.backgroundPosition = "center";
+    setLinks("https://cavanagh.dev/Chess/", "https://github.com/ryan-w-c/chess", "Play Now", "View on Github");
+    setText("Chess", "A chess game with special move, check, checkmate, and stalemate logic.");
+}
 
 function elevator() {
     openNav();
     setCards('url(img/elev.png)', 'url(img/elevator.png)');
-    setLinks("https://github.com/KRSung/Elevator_3.0", "https://github.com/ryan-w-c/bingo", "View on Github");
+    setLinks("https://github.com/KRSung/Elevator_3.0", "", "View on Github");
     link2.style.visibility = "hidden";
-    setText("Elevator Simulator", "This project simulates an elevator. It is capable of changing the number of floors, elevators, and seed, as well as, allowing different types of passengers. It taught the core concepts of object-oriented programming and was completed with a partner as a class project.");
-    createInfo("Java");
-  }
+    setText("Elevator Simulator", "Simulates an elevator with multiple floors, evelvators, passengers, etc.");
+}
 
-function bingoDraw() {
+function crypto() {
     openNav();
-    setCards('url(img/bingoNum.png)', 'url(img/board.png)');
-    setLinks("https://cavanagh.dev/bingoBoard/", "https://github.com/ryan-w-c/bingoBoard", "Play Now");
-    setText("Bingo Number Draw", "This site draws random numbers for a bingo game and displays them on a bingo board.");
-    createInfo("JavaScript");
-    createInfo("HTML");
-    createInfo("CSS");
-  }
-
-function rgb() {
-    openNav();
-    setCards('url(img/rgbPic.png)', 'url(img/rgb2.png)');
-    setLinks("https://github.com/ryan-w-c/RGB-Image-Generator/releases/download/v1.0.0/RGB-Image_Generator.zip", "https://github.com/ryan-w-c/RGB-Image-Generator", "Download Zip (Windows)");
-    setText("RGB-Image-Generator", "This program reads a user selected image and stores each RGB value. A new one pixel length image is created using the stored RGB values. The new image is then stretched to the desired output ratio.");
-    createInfo("Python");
-    createInfo("Kivy");
-    createInfo("PIL");
-  }
-
-function ticTacToe() {
-    openNav();
-    setCards('url(img/tic2.png)', 'url(img/tic1.png)');
+    setCards('url(img/crypto1.png)', 'url(img/crypto2.png)');
     pic1.style.backgroundPosition = "center";
-    setLinks("https://cavanagh.dev/TicTacToe", "https://github.com/ryan-w-c/TicTacToe", "Play Now");
-    setText("Tic-Tac-Toe", "This game is a basic 3-in-a-row or 5-in-a-row, turn based, tic-tac-toe game.");
-    createInfo("C#");
-    createInfo("Unity Game Engine");
-  }
+    setLinks("https://crypto-prices-webapp.herokuapp.com/", "https://github.com/ryan-w-c/crypto-prices", "Try it");
+    setText("Crypto Prices API Webapp", "Compares BTC and ETH prices from two exchanges. The site may take between 30-60 seconds to load on launch.");
+}
 
-function createInfo(codeUsed) {
-  info = document.createElement("p");
-  info.className = "code";
-  info.innerHTML = "&bull; " + codeUsed;
-  codeInfo.appendChild(info);
-  }
+function cal() {
+    openNav();
+    setCards('url(img/cal1.png)', 'url(img/cal2.png)');
+    pic1.style.backgroundPosition = "center";
+    setLinks("https://github.com/ryan-w-c/LaunchMyMeeting", "", "View on Github");
+    link2.style.visibility = "hidden";
+    setText("MacOS Meeting Launcher", "Launches virtual meetings with one tap.");
+}
 
-function contact(item) {
+function hugMe() {
+    openNav();
+    setCards('url(img/hugMeCampus1.png)', 'url(img/hugMeCampus2.png)');
+    setLinks("https://github.com/ryan-w-c/hugMeCampus", "", "View on Github");
+    link2.style.visibility = "hidden";
+    setText("HugMe Campus Android Application", "Tinder like application for finding hugs on a college campus. Features matchmaking, search, and messaging.");
+}
+
+function contact() {
     formNav.style.width = "100%";
     formNav.scrollTo(0, 0);
-    // fname.value = "";
-    // email.value = "";
-    // company.value = "";
-    // message.value = "";
-    disable(start);
-    enable(form);
-  }
+}
 
-function closeContact(item) {
+function closeContact() {
     formNav.style.width = "0%";
-    disable(form);
-    enable(start);
   }
